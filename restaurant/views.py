7ebfa249f3ca90a315876d28462bf062009441f3
Baptiste Jacquin <baptiste.jacquin@campus-numerique-in-the-alps.com>
2020-01-17 15:59:50 +0100

    Feat : Ajout de la fonctionnalité d'affichage de la liste de restaurants

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+from restaurant.models import Restaurant
+
+
+def index(request):
+    template = loader.get_template("restaurants/list.html")
+    restaurants = Restaurant.objects.all()
+    context = {"restaurants": restaurants}
+    return HttpResponse(template.render(context, request))
