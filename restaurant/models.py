891237bbee4f2efd02d73c11ba91afd00b25cfa7
Baptiste Jacquin <baptiste.jacquin@campus-numerique-in-the-alps.com>
2020-01-17 15:53:05 +0100

    Feat : Ajout du model restaurant et sa migration

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=40)
+    adress = models.CharField(max_length=60)
+    email = models.EmailField()
+    phone = models.PositiveIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
